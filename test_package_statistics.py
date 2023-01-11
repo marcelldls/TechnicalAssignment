519d8b703b4a6603258f2c8cb4faef7b44ce0d9d
262882 <2628829@students.wits.ac.za>
2023-01-11 12:14:16 +0200

    Set up tox to run tests on a set of python3 versions
    Approx 1 hour of work

--- test_package_statistics.py
+++ test_package_statistics.py
@@ -6,23 +6,23 @@ import unittest
 import os
 from package_statistics import download_cf
 
-test_architecture = 'amd64'
-test_file = 'Contents-' + test_architecture + '.gz'
-
 class TestDownloadCf(unittest.TestCase):
     """
     Only using amd64 otherwise test duration is long (each architecture ~10mb files)
     """
+    test_architecture = 'amd64'
+    test_file = 'Contents-' + test_architecture + '.gz'
 
     def setUp(self):
-        if os.path.exists(test_file):
-            os.remove(test_file) 
+        if os.path.exists(self.test_file):
+            os.remove(self.test_file)
 
     def test_download(self):
-        download_cf(test_architecture)
-        self.assertTrue(os.path.exists(test_file))
-        if os.path.exists(test_file):
-            os.remove(test_file) 
+        """Test downloading of contents file"""
+        download_cf(self.test_architecture)
+        self.assertTrue(os.path.exists(self.test_file))
+        if os.path.exists(self.test_file):
+            os.remove(self.test_file)
 
 if __name__ == '__main__':
     unittest.main()
