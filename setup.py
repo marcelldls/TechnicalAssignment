0000d53b4430b865f385ef04d65902a849e7ec2f
Marcell Nagy <marcell.nagy@diamond.ac.uk>
2024-01-09 18:02:48 +0000

    Add python requirements

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 from setuptools import setup
 
 setup(
+    name = 'package_statistics',
     py_modules=['package_statistics'],
+    python_requires='>=3.8',
     extras_require={
         'dev': [
             'tox',
