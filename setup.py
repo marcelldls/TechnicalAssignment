489dbbfe4c1d3e5d671db7d14ed11cbb850a5f67
Marcell Nagy <marcell.nagy@diamond.ac.uk>
2024-01-12 09:55:11 +0000

    Migrate to setup.cfg

--- setup.py
+++ setup.py
@@ -1,18 +1,4 @@
 from setuptools import setup
 
-setup(
-    name = 'package_statistics',
-    py_modules=['package_statistics'],
-    python_requires='>=3.8',
-    extras_require={
-        'dev': [
-            'black',
-            'tox',
-        ],
-    },
-    entry_points={
-        'console_scripts': [
-            'package_statistics = package_statistics:main',
-        ],
-    },
-)
+if __name__ == "__main__":
+    setup()
