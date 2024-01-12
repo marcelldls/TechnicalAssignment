cd386abce5f102e90662001c9efbf4d26ec2506f
Marcell Nagy <marcell.nagy@diamond.ac.uk>
2024-01-12 17:05:07 +0000

    Perform linting

--- package_statistics.py
+++ package_statistics.py
@@ -1,57 +1,60 @@
-#!/usr/bin/env python3
-
 """
-A python command line tool that takes architecture (amd64, arm64, etc.) as an argument and outputs
-the statistics of the top 10 packages (from Debian mirror:
-http://ftp.uk.debian.org/debian/dists/stable/main/) that have the most files associated with them.
+A python command line tool that takes architecture (amd64, arm64, etc.) as an 
+argument and outputs the statistics of the top 10 packages from a Debian 
+mirror that have the most files associated with them.
 """
 
 import argparse
+import gzip
 import logging
 import os
-import urllib.request
-import urllib.error
-import gzip
 import shutil
 import tempfile
+import urllib.error
+import urllib.request
 
 
-def download_cf(architecture: str, debian_mirror:str, working_dir: str) -> None:
+def download_cf(
+    architecture: str, debian_mirror: str, working_dir: str
+) -> None:
     """Download architecture contents file from the debian mirror"""
 
-    contents_file = 'Contents-' + architecture + '.gz'
+    contents_file = "Contents-" + architecture + ".gz"
     path = os.path.join(working_dir, contents_file)
     remote_url = debian_mirror + contents_file
 
-    try:   # Download file and save locally
-        logging.info('Retrieving file: ' + contents_file)
+    try:  # Download file and save locally
+        logging.info("Retrieving file: " + contents_file)
         urllib.request.urlretrieve(remote_url, path)
-        logging.info('Contents file sucessfully retrieved!')
+        logging.info("Contents file sucessfully retrieved!")
     except urllib.error.HTTPError as e:
-        raise Exception(f"{e}. Is '{architecture}' a valid architecture?") from e
+        raise Exception(
+            f"{e}. Is '{architecture}' a valid architecture?"
+        ) from e
     except OSError as e:
         raise Exception(f"{e}: Is your internet connection active?") from e
+
 
 def decompress_cf(architecture: str, dir: str) -> None:
     """Decompresses an architecture contents file"""
 
-    contents_file = 'Contents-' + architecture
-    compressed_file = os.path.join(dir, contents_file + '.gz')
+    contents_file = "Contents-" + architecture
+    compressed_file = os.path.join(dir, contents_file + ".gz")
     decompressed_file = os.path.join(dir, contents_file)
 
     # Decompress file
-    logging.info('Decompressing contents file')
-    with gzip.open(compressed_file, 'rb') as f_in:
-        with open(decompressed_file, 'wb') as f_out:
+    logging.info("Decompressing contents file")
+    with gzip.open(compressed_file, "rb") as f_in:
+        with open(decompressed_file, "wb") as f_out:
             shutil.copyfileobj(f_in, f_out)
-    logging.info('Contents file decompressed')
+    logging.info("Contents file decompressed")
 
 
 class CfStatistics:
     """Process statistics of an architecture contents file"""
 
     def __init__(self, architecture, dir):
-        self.package_dict = {}     # Dictionary has O(1) search vs list O(n)
+        self.package_dict = {}  # Dictionary has O(1) search vs list O(n)
         self.package_names = []
         self.file_count = []
         self.architecture = architecture
@@ -61,46 +64,49 @@ class CfStatistics:
 
     def __tally(self):
         # load file
-        file_name = 'Contents-' + self.architecture
+        file_name = "Contents-" + self.architecture
         path = os.path.join(self.dir, file_name)
         logging.info("Parsing contents file")
-        with open(path, 'rt', errors='ignore') as file:
-
+        with open(path, "rt", errors="ignore") as file:
             # Search For header
             start_line = 0
             for i in range(100):
                 line = file.readline()
                 if "".join(line.split()) == "FILELOCATION":
                     start_line = i
-            logging.info("Starting scan from line" + ' ' + str(start_line))
+            logging.info("Starting scan from line" + " " + str(start_line))
 
-        with open(path, 'rt', errors='ignore') as file:
-
+        with open(path, "rt", errors="ignore") as file:
             # Scan each line
-            for num, line in enumerate(file):
-
+            for num, raw_line in enumerate(file):
                 # Skip line if before or is header
-                if num <= start_line-1:
+                if num <= start_line - 1:
                     continue
 
                 # Extract name as string
-                line = line.strip('\n')
-                name_idx = line.rfind('/')      # Find index where name starts
-                line_name = line[name_idx+1:]   # Slice out name
+                line = raw_line.strip("\n")
+                name_idx = line.rfind("/")  # Find index where name starts
+                line_name = line[name_idx + 1 :]  # Slice out name
 
                 # Populate dictionary with unique names and occurances
                 if line_name not in self.package_dict:
-                    self.package_dict[line_name] = 1     # unique key
+                    self.package_dict[line_name] = 1  # unique key
                 else:
-                    self.package_dict[line_name] += 1    # existing key
+                    self.package_dict[line_name] += 1  # existing key
 
     def __sort(self):
-        sorted_dict = sorted(self.package_dict.items(), key=lambda item: item[1])  # list of tuples
-        self.file_count = [sorted_dict[-(i+1)][1] for i in range(len(sorted_dict))]
-        self.package_names = [sorted_dict[-(i+1)][0] for i in range(len(sorted_dict))]
+        sorted_dict = sorted(
+            self.package_dict.items(), key=lambda item: item[1]
+        )  # list of tuples
+        self.file_count = [
+            sorted_dict[-(i + 1)][1] for i in range(len(sorted_dict))
+        ]
+        self.package_names = [
+            sorted_dict[-(i + 1)][0] for i in range(len(sorted_dict))
+        ]
 
     def print_top10(self):
-        print('The top 10 packages with the highest file counts are:')
+        print("The top 10 packages with the highest file counts are:")
         for i in range(10):
             try:
                 print(f"{i+1}. {self.package_names[i]}  {self.file_count[i]}")
@@ -109,34 +115,37 @@ class CfStatistics:
 
 
 def main():
-
     DEB_MIRROR = "http://ftp.uk.debian.org/debian/dists/stable/main/"
 
     # Process arguments
-    parser = argparse.ArgumentParser(prog='package_statistics.py')
+    parser = argparse.ArgumentParser(
+        prog="package_statistics.py", description=__doc__
+    )
     parser.add_argument(
-        '-v', '--verbose',
+        "-v",
+        "--verbose",
         help="verbose output",
-        action="store_const", dest="loglevel", const=logging.INFO,
+        action="store_const",
+        dest="loglevel",
+        const=logging.INFO,
         default=logging.WARNING,
     )
     parser.add_argument(
         "arch",
         metavar="architecture",
         help="desired architecture (amd64, arm64, etc.)",
-        )
+    )
     parser.add_argument(
         "--debian_mirror",
-        metavar='',
+        metavar="",
         default=DEB_MIRROR,
         help=f"desired Debian Mirror (Default: {DEB_MIRROR})",
-        )
+    )
     args = parser.parse_args()
-    logging.basicConfig(level=args.loglevel, format='%(message)s')
+    logging.basicConfig(level=args.loglevel, format="%(message)s")
     print(f"Processing package statistics for {args.arch}...")
 
     with tempfile.TemporaryDirectory() as tmpdirname:
-
         # Aquire contents file in working directory
         download_cf(args.arch, args.debian_mirror, tmpdirname)
         decompress_cf(args.arch, tmpdirname)
--- tests/test_package_statistics.py
+++ tests/test_package_statistics.py
@@ -2,30 +2,37 @@
 
 """Tests for package_statistics"""
 
-import unittest
-import os
 import gzip
+import os
 import shutil
-import sys
-sys.path.append('../')
-from package_statistics import read_args, download_cf, decompress_cf, cleanup_cf, CfStatistics
+import unittest
+
+from package_statistics import (
+    CfStatistics,
+    cleanup_cf,
+    decompress_cf,
+    download_cf,
+    read_args,
+)
+
 
 class TestReadArgs(unittest.TestCase):
     """
     Test processing of user arguments
     """
-    test_architecture = 'amd64'
-    test_file = 'Contents-' + test_architecture + '.gz'
+
+    test_architecture = "amd64"
+    test_file = "Contents-" + test_architecture + ".gz"
 
     def test_two(self):
         """Test processing of two arguments"""
-        sys_args1 = ['./package_statistics.py', 'amd64']
+        sys_args1 = ["./package_statistics.py", "amd64"]
         args_1 = read_args(sys_args1)
-        self.assertTrue(args_1 == 'amd64')
+        self.assertTrue(args_1 == "amd64")
 
     def test_few(self):
         """Test processing of too few arguments"""
-        sys_args2 = ['./package_statistics.py']
+        sys_args2 = ["./package_statistics.py"]
         result_few = False
         try:
             read_args(sys_args2)
@@ -35,7 +42,7 @@ class TestReadArgs(unittest.TestCase):
 
     def test_many(self):
         """Test processing of too many arguments"""
-        sys_args3 = ['./package_statistics.py', 'amd64', 'arm64']
+        sys_args3 = ["./package_statistics.py", "amd64", "arm64"]
         result_many = False
         try:
             read_args(sys_args3)
@@ -43,15 +50,17 @@ class TestReadArgs(unittest.TestCase):
             result_many = True
         self.assertTrue(result_many)
 
+
 class TestDownloadCf(unittest.TestCase):
     """
     Test downloading of contents file
     Only using amd64 otherwise test duration is long
     """
-    test_architecture = 'amd64'
-    test_file = 'Contents-' + test_architecture + '.gz'
 
-    def setUp(self): # Remove any preexisting contents file
+    test_architecture = "amd64"
+    test_file = "Contents-" + test_architecture + ".gz"
+
+    def setUp(self):  # Remove any preexisting contents file
         if os.path.exists(self.test_file):
             os.remove(self.test_file)
 
@@ -59,47 +68,54 @@ class TestDownloadCf(unittest.TestCase):
         """Test downloading of contents file"""
         download_cf(self.test_architecture)
         self.assertTrue(os.path.exists(self.test_file))
-        if os.path.exists(self.test_file):   # Clean up
+        if os.path.exists(self.test_file):  # Clean up
             os.remove(self.test_file)
+
 
 class TestDecompressCf(unittest.TestCase):
     """
     Test decompression of contents file
     """
-    test_architecture = 'amd64'
-    decompressed_file = 'Contents-' + test_architecture
 
-    def setUp(self): # Remove any preexisting contents file
+    test_architecture = "amd64"
+    decompressed_file = "Contents-" + test_architecture
+
+    def setUp(self):  # Remove any preexisting contents file
         if os.path.exists(self.decompressed_file):
             os.remove(self.decompressed_file)
 
     def test_decompress(self):
         """Test decompression of contents file"""
-        open(self.decompressed_file, 'w').close()    # Create empty file
+        open(self.decompressed_file, "w").close()  # Create empty file
 
         # Compress file
-        with open(self.decompressed_file, 'rb') as f_in:
-            with gzip.open(self.decompressed_file+'.gz', 'wb') as f_out:
+        with open(self.decompressed_file, "rb") as f_in:
+            with gzip.open(self.decompressed_file + ".gz", "wb") as f_out:
                 shutil.copyfileobj(f_in, f_out)
-        os.remove(self.decompressed_file) # Clean up by deleting uncompressed file
+        os.remove(
+            self.decompressed_file
+        )  # Clean up by deleting uncompressed file
 
         decompress_cf(self.test_architecture)
         self.assertTrue(os.path.exists(self.decompressed_file))
         if os.path.exists(self.decompressed_file):  # Clean up
             os.remove(self.decompressed_file)
 
+
 class TestCleanup(unittest.TestCase):
     """
     Test cleaning up of contents file
     """
-    test_architecture = 'amd64'
-    target_file = 'Contents-' + test_architecture
+
+    test_architecture = "amd64"
+    target_file = "Contents-" + test_architecture
 
     def test_cleanup(self):
         """Test cleaning up of contents file"""
-        open(self.target_file, 'w').close()    # Create empty file
+        open(self.target_file, "w").close()  # Create empty file
         cleanup_cf(self.test_architecture)
         self.assertFalse(os.path.exists(self.target_file))
+
 
 class TestCf1Statistics(unittest.TestCase):
     """
@@ -107,9 +123,22 @@ class TestCf1Statistics(unittest.TestCase):
     """
 
     def setUp(self):
-        self.expect_count = [17, 14, 12, 10, 8, 7, 6, 5, 4, 3]  # Manually counted
-        self.expect_names = ['package_'+str(x+1) for x in range(10)] # Expected package rank
-        self.test1_stats = CfStatistics('test1')
+        self.expect_count = [
+            17,
+            14,
+            12,
+            10,
+            8,
+            7,
+            6,
+            5,
+            4,
+            3,
+        ]  # Manually counted
+        self.expect_names = [
+            "package_" + str(x + 1) for x in range(10)
+        ]  # Expected package rank
+        self.test1_stats = CfStatistics("test1")
 
     def test1_count(self):
         """Test if file count is correct for the test1 format example"""
@@ -119,15 +148,29 @@ class TestCf1Statistics(unittest.TestCase):
         """Test if rank is correct for the test1 format example"""
         self.assertTrue(self.test1_stats.package_names == self.expect_names)
 
+
 class TestCf2Statistics(unittest.TestCase):
     """
     Test parsing of the contents file using test2 contents file format
     """
 
     def setUp(self):
-        self.expect_count = [17, 14, 12, 10, 8, 7, 6, 5, 4, 3]  # Manually counted
-        self.expect_names = ['package_'+str(x+1) for x in range(10)] # Expected package rank
-        self.test2_stats = CfStatistics('test2')
+        self.expect_count = [
+            17,
+            14,
+            12,
+            10,
+            8,
+            7,
+            6,
+            5,
+            4,
+            3,
+        ]  # Manually counted
+        self.expect_names = [
+            "package_" + str(x + 1) for x in range(10)
+        ]  # Expected package rank
+        self.test2_stats = CfStatistics("test2")
 
     def test2_count(self):
         """Test if file count is correct for the test2 format example"""
@@ -137,5 +180,6 @@ class TestCf2Statistics(unittest.TestCase):
         """Test if rank is correct for the test2 format example"""
         self.assertTrue(self.test2_stats.package_names == self.expect_names)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
